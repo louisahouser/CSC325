db0f8ff232b6090a4c577fb87aaa4b3611271579
Louisa Houser <louisahouser@gmail.com>
2024-02-20 01:22:54 +0000

    added random number generator app

--- random_num_gen/lib/main.dart
+++ random_num_gen/lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'dart:math';
 
@@ -12,25 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: RandomNumberGenerator(),
+      home: const RandomNumberGenerator(),
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme:
-            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 33, 120, 219)),
+            ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 39, 25, 238)),
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
@@ -39,6 +25,8 @@ class MyApp extends StatelessWidget {
 }
 
 class RandomNumberGenerator extends StatefulWidget {
+  const RandomNumberGenerator({super.key});
+
   @override
   _RandomNumberGeneratorState createState() => _RandomNumberGeneratorState();
 }
@@ -49,7 +37,7 @@ class _RandomNumberGeneratorState extends State<RandomNumberGenerator> {
 
   void generateRandomNumber() {
     setState(() {
-      _randomNumber = 1000 + _random.nextInt(9000);
+      _randomNumber = 10000000 + _random.nextInt(90000000);
 
       /// generates random 4 digit number
     });
@@ -59,24 +47,32 @@ class _RandomNumberGeneratorState extends State<RandomNumberGenerator> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Random 4-Digit Number Generator'),
+        title: const Text('Random 8-Digit Number Generator'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'Random 4-Digit Number:',
-              style: TextStyle(fontSize: 20),
+            const Text(
+              'Random 8-Digit Number:',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800),
             ),
             Text(
               '$_randomNumber', // display generated number
-              style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+              style:
+                  const TextStyle(fontSize: 144, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: generateRandomNumber,
-              child: Text('Generate Random 4-Digit Number'),
+              style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(
+                      Color.fromARGB(255, 245, 41, 194)),
+                  foregroundColor: MaterialStatePropertyAll(
+                      Color.fromARGB(255, 254, 254, 255)),
+                  mouseCursor: MaterialStatePropertyAll(MouseCursor.defer),
+                  shape: MaterialStatePropertyAll(BeveledRectangleBorder())),
+              child: const Text('Generate Random 8-Digit Number'),
             ),
           ],
         ),
